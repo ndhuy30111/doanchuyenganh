2ef8f2d3e8be796e472d14c456dc35155e2b210c
ndhuy30111 <dh51701452@student.stu.edu.vn>
2020-12-18 22:15:29 +0700

    Fix Header

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,5 +26,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        $dataheader = DB::table('category')->whereNull('categoryid')->get();
+        View::share(['dataheader' => $dataheader]);
     }
 }
